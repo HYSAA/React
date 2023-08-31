095c9d5e6c2182e1268763b951640c177de6812c
HYSA <aj.anunciado@gmail.com>
2023-09-01 02:53:49 +0800

    aaa

--- static app/index.js
+++ static app/index.js
@@ -1,1 +1,17 @@
-ReactDom.render(<h1>Hello</h1>,document.getElementById("root"))
+// Challenge: Create your own custom React component!
+// Call it "MainContent", and have it return a simple
+// h1 element that says "I'm learning React!"
+
+// Afterward, render it below the Navbar (which
+// you can do inside the ReactDOM.render call below)
+
+const Navbar=() =>{
+return
+(
+    <h1>Im learning react!</h1>
+)
+}
+
+ReactDOM.render(
+    <div><Navbar/></div>,document.getElementById("root")
+)
