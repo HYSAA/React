b4ccc7df54bccbc5e89b315f7d4296fe0d9446a6
HYSA <aj.anunciado@gmail.com>
2023-09-01 21:21:05 +0800

    wakoy nagets

--- static app/index.js
+++ static app/index.js
@@ -1,16 +1,44 @@
 import ReactDOM from "react-dom/client"
 import React from "react"
 const page=(
-    <nav>
-        <h1>MY WEBSITE</h1>
-        <ul className="pricing">Pricing</ul>
-        <ul className="About">About</ul>
-        <ul className="Contact">Contact</ul>
-        <ul className="Age">Age</ul>
-    </nav>
+    /**
+Challenge: find out what happens if we try to append JSX
+to our div#root using .append() instead of ReactDOM
+
+1. Create a sample page in JSX (≥ 4 elements) and save them in a variable
+2. Select the div with the ID of "root" and use `.append()` to append
+   your JSX
+3. See if you can guess what will show up in the browser before running
+   the code
+4. See if you can explain what actually shows up in the browser
+ */
+   <div>
+    <h1>My awesome website</h1>
+    <h3>
+        h3 paragraph
+    </h3>
+    <ol>
+        <li>
+            I miss u
+        </li>
+        <li>
+            I love u
+        </li>
+        <li>
+            I adore u
+        </li>
+    </ol>
+   
+
+
+   </div>
+
+
 )
+
+document.getElementById("root").append(page)
 //  ReactDOM.render(
 //     page,document.getElementById("root")
 //  )
-ReactDOM.createRoot(document.getElementById("root")).render(page)
+// ReactDOM.createRoot(document.getElementById("root")).render(page)
 
