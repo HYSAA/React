6e9d2fb1ec84ad3a84172535a24113485ca2c9c0
HYSA <aj.anunciado@gmail.com>
2023-09-01 20:34:47 +0800

    react

--- first-site/src/App.js
+++ first-site/src/App.js
@@ -1,16 +1,39 @@
  
 import './App.css';
 
-function App() {
+import React from 'react';
+import { ReactDOM } from 'react-dom';
+
+function navbar() {
   return (
+    <div className='forms'>
     <nav>
     <h1>MY WEBSITE</h1>
     <ul className="pricing">Pricing</ul>
     <ul className="About">About</ul>
     <ul className="Contact">Contact</ul>
+    
+    
 
 </nav>
+</div>
   );
 }
+// const test = () =>{
+// return(
+//    <div className='test'>
+//    <p>Hello</p>
 
-export default App;
+//    </div>
+
+// )
+
+// }
+
+// export default App;
+
+
+
+// ReactDOM.render(navbar,document,getElementbyId("root"))
+// export default test
+export default navbar
