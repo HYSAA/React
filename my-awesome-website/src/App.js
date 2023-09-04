2ebc67dd39d2487404f067ad4b983bb8bfd2410b
HYSA <aj.anunciado@gmail.com>
2023-09-04 19:14:49 +0800

    retrun

---
+++ my-awesome-website/src/App.js
@@ -0,0 +1,20 @@
+import { ReactDOM } from 'react-dom';
+import './App.css';
+import React from 'react';
+function App() {
+  return (
+    <div>
+    <ul>
+      <img src="./react logo.png"/>
+      <h1>Fun Facts about react</h1>
+      <li>was first released in 2013</li>
+      <li>was originally created by Jordan Walke</li>
+      <li>has well over 100k stars on Github</li>
+      <li>is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps,including mobile apps</li>
+    </ul>
+    </div>
+  );
+}
+
+export default App;
