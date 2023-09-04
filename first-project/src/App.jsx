e1201bf243a09fe5a5f57438821206ac2b09a1c3
HYSA <aj.anunciado@gmail.com>
2023-09-05 05:08:34 +0800

    SDSD

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -1,24 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import reactLogo from "./assets/react.svg"
+/**
+Challenge: 
 
-function App() {
-  const [count, setCount] = useState(0)
+Part 2: 
+- Add a `header` element with a nested `nav` element. Inside the `nav`,
+  include a `img` element with the image of the React logo inside
+  (src="./react-logo.png") and make sure to set the width to something
+  more manageable so it doesn't take up the whole screen
+- Add an `h1` with some text describing the page. (E.g. "Reasons
+  I'm excited to learn React"). Place it above the ordered list.
+- Add a `footer` after the list that says: 
+    "© 20xx <last name here> development. All rights reserved."
 
-  return (
-     <div className='menu'>
+ */
+function App(){
+return(
+
+<div>
+  <header>
+    <nav>
+    <img src={reactLogo} width={100} alt="react logo"/>
+    </nav>
+  </header>
     <ul>
-      <img src={reactLogo} width={100} alt="react logo"/>
-      <h1>Fun Facts about react</h1>
-      <li>was first released in 2013</li>
-      <li>was originally created by Jordan Walke</li>
-      <li>has well over 100k stars on Github</li>
-      <li>is maintained by Facebook</li>
-      <li>Powers thousands of enterprise apps,including mobile apps</li>
+   
+    <h1>Fun Facts about react</h1>
+    <li>was first released in 2013</li>
+    <li>was originally created by Jordan Walke</li>
+    <li>has well over 100k stars on Github</li>
+    <li>is maintained by Facebook</li>
+    <li>Powers thousands of enterprise apps,including mobile apps</li>
     </ul>
-    </div>
-  )
+</div>
+)
 }
-
 export default App
+
+
+
--- static app/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-let app= document.getElementById("app")
- 
-app.textContent="asdasdas"
-app.className="header"
-document.getElementById("root").append(app)
-
