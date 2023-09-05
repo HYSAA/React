9cb2e8fed36a8f02a0f8bfae8852582b11269320
HYSA <aj.anunciado@gmail.com>
2023-09-05 21:59:51 +0800

    Changed to vite

---
+++ scrimba-first/src/App.js
@@ -0,0 +1,52 @@
+import logo from './logo.svg';
+import './App.css';
+import ReactLogo from './react-logo.png'
+
+function Header(){
+return(
+  <header>
+    <nav>
+    <img src={ReactLogo} width="100px" />
+    <ul classname="Menu">
+    
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+    </nav>
+  </header>
+)
+}
+
+function Body() {
+  return (
+  <div>
+
+   
+  <h1>Reasons I'm excited to learn React</h1>
+  <ul>
+    <li>1.It's a popular library</li>
+    <li>2.I'm more likely to get a job as a developer if I know React</li>
+  </ul>
+
+  </div>   
+  );
+}
+
+function Footer(){
+return(
+  <small>© 2021 Ziroll development. All rights reserved.</small>
+)
+}
+
+function Page(){
+return(
+  <div>
+<Header></Header>
+<Body> </Body>
+<Footer></Footer>
+</div>
+)
+}
+
+export default Page;
