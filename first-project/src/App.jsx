43455be9cc61b2ad32de20a330be2f1ee1e32b8c
HYSA <aj.anunciado@gmail.com>
2023-09-07 06:30:17 +0800

    asdasd

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -1,36 +1,45 @@
 import './App.css'
-import reactLogo from "./assets/react-logo.png"
 import React from 'react'
 // import Header from './Header.jsx'
-
-function Header(){
- return(
-<header>
-<nav>
-<img src={reactLogo} width="200px" />
-</nav>
-</header>
-  )
-  }
+// function Header(){
+//  return(
+// <header>
+// <nav>
+// <img src={reactLogo} width="200px" />
+// </nav>
+// </header>
+//   )
+//   }
   
-function App(){
+// function App(){
+// return(
+// <div>
+//   <Header ></Header>
+//     <ul>
+//     <h1>Fun Facts about react</h1>
+//     <li>was first released in 2013</li>
+//     <li>was originally created by Jordan Walke</li>
+//     <li>has well over 100k stars on Github</li>
+//     <li>is maintained by Facebook</li>
+//     <li>Powers thousands of enterprise apps,including mobile apps</li>
+//     </ul>
+//     <Practice></Practice>
+//     <footer>I miss u</footer>
+// </div>
+// )
+// }
+
+function Practice(){
 return(
-<div>
-  <Header ></Header>
-    <ul>
-    <h1>Fun Facts about react</h1>
-    <li>was first released in 2013</li>
-    <li>was originally created by Jordan Walke</li>
-    <li>has well over 100k stars on Github</li>
-    <li>is maintained by Facebook</li>
-    <li>Powers thousands of enterprise apps,including mobile apps</li>
-    </ul>
-    <footer>I miss u</footer>
-</div>
+<nav className="container">
+            <div>Home</div>
+            <div>Search</div>
+            <div>Logout</div>
+        </nav>
 )
 }
 
-export default App
+export default Practice
 
 
 
