3ae40fcc4ea6c451f8a7dc83a51cfba67b3de327
HYSA <aj.anunciado@gmail.com>
2023-09-08 05:45:29 +0800

    made a addition calculator in reaact :)

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -1,5 +1,5 @@
-import './App.css'
-import React from 'react'
+// import './App.css'
+// import React from 'react'
 // import Header from './Header.jsx'
 // function Header(){
 //  return(
@@ -29,17 +29,58 @@ import React from 'react'
 // )
 // }
 
-function Practice(){
-return(
-<nav className="container">
-            <div>Home</div>
-            <div>Search</div>
-            <div>Logout</div>
-        </nav>
-)
+// function Practice(){
+// return(
+// <nav className="container">
+//             <div>Home</div>
+//             <div>Search</div>
+//             <div>Logout</div>
+//         </nav>
+// )
+// }
+
+// export default Practice
+
+import React, { useState } from "react";
+
+
+const App = () =>{
+
+  const[firstNumber,setFirstNumber]=useState("")
+  const[secondNumber,setSecondNumber]=useState("")
+  let [sum,setSum]=useState("")
+  
+  const Addition= () =>{
+  const a= parseInt(firstNumber);
+  const b= parseInt(secondNumber);
+  setSum(a+b);
+  console.log(setSum)
+
+
+  }
+
+  return(
+  <div className="Addition">
+  <h1>Enter First Number</h1>
+  <input
+ type="number"
+ placeholder="First Number"
+ onChange={(event)=>setFirstNumber(event.target.value)}
+  />
+  <br/>
+
+  <h2>Enter Second Number</h2>
+  <input type="number" placeholder="Second Number" 
+  onChange={(event)=>setSecondNumber(event.target.value)}/>
+ 
+
+ <button onClick={Addition}>Add</button>
+ <h2> Result is: {sum} </h2>
+
+
+</div>
+  )
+
 }
 
-export default Practice
-
-
-
+export default App;
