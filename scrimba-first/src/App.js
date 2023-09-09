9279e76028f471ef39b73f81fdaf283ee0c640a2
HYSA <aj.anunciado@gmail.com>
2023-09-09 19:21:43 +0800

    dsd

--- scrimba-first/src/App.js
+++ scrimba-first/src/App.js
@@ -25,7 +25,7 @@ function Body() {
    
   <h1>Reasons I'm excited to learn React</h1>
   <ul className="Blist">
-    <li>1.It's a popular library</li>
+    <li>1.It's a popular library</li> 
     <li>2.I'm more likely to get a job as a developer if I know React</li>
   </ul>
 
