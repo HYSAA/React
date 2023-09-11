c8f6c067bb5b0597eef7b5cbc57061778bc8f9ba
Alberto Anunciado Jr <aj.anunciado@gmail.com>
2023-09-11 20:00:24 +0800

    s

--- first-site/src/App.jsx
+++ first-site/src/App.jsx
@@ -4,13 +4,14 @@ import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Body from './Body.jsx'
 
-function Page(){
-return(
-  <div className='page'>
-<Header></Header>
-<Body></Body>
-<Footer></Footer>
-</div>
-)
+function Page() {
+  return (
+    //added a div lol
+    <div className='page'>
+      <Header></Header>
+      <Body></Body>
+      <Footer></Footer>
+    </div>
+  )
 }
 export default Page;
