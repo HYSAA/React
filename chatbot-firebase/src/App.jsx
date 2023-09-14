ff9fede9d94172372c3fdb6f20615923c2d75723
Alberto Anunciado Jr <aj.anunciado@gmail.com>
2023-09-15 05:45:00 +0800

    work:

--- chatbot-firebase/src/App.jsx
+++ chatbot-firebase/src/App.jsx
@@ -91,7 +91,7 @@ firebase.initializeApp({
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
-const analytics = firebase.analytics();
+// const analytics = firebase.analytics();
 
 
 function App() {
