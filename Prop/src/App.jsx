9206f7104e6061ce254879c208d76520400d6ce1
Alberto Anunciado Jr <aj.anunciado@gmail.com>
2023-10-04 07:08:33 +0800

    asdasD

--- Prop/src/App.jsx
+++ Prop/src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
-
+import Contact from './Contact'
 function App() {
  
   return (
     <>
-    <App></App>
+    <Contact img src="./images/mr-whiskerson.png" alt="Mr. Whiskerson" 
+     name="Mr. Whiskerson" phone="23123" email="aj,anunciado@gmail.com">
+
+
+    </Contact>
+    <Contact></Contact>
+    <Contact></Contact>
+    <Contact></Contact>
     </>
   )
 }
---
+++ Prop/src/Contact.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <div>
+      <div className="contacts">
+        <div className="contact-card">
+          <img src="./images/mr-whiskerson.png" alt="Mr. Whiskerson" />
+          <h3>Mr. Whiskerson</h3>
+          <div className="info-group">
+            <img src="./images/phone-icon.png" alt="Phone Icon" />
+            <p>(212) 555-1234</p>
+          </div>
+          <div className="info-group">
+            <img src="./images/mail-icon.png" alt="Mail Icon" />
+            <p>mr.whiskaz@catnap.meow</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
